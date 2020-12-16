5a0104e055a4d12b68af7982a2834d90755f2a2a
yuninguyen <yuninguyen.it@gmail.com>
2020-12-16 22:45:35 +0700

    commit code

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -1,59 +1,55 @@
 <?php
-  
+
 namespace App\Http\Controllers\Auth;
-  
+
 use App\Http\Controllers\Controller;
 use Socialite;
 use Auth;
 use Exception;
 use App\User;
-  
+
 class GoogleController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Redirect the user to the Google authentication page.
      *
-     * @return void
+     * @return \Illuminate\Http\Response
      */
     public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
-      
     /**
-     * Create a new controller instance.
+     * Obtain the user information from Google.
      *
-     * @return void
+     * @return \Illuminate\Http\Response
      */
     public function handleGoogleCallback()
     {
         try {
-    
             $user = Socialite::driver('google')->user();
-     
-            $finduser = User::where('google_id', $user->id)->first();
-     
-            if($finduser){
-     
-                Auth::login($finduser);
-    
-                return redirect('/home');
-     
-            }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
-                ]);
-    
-                Auth::login($newUser);
-     
-                return redirect('/home');
-            }
-    
         } catch (Exception $e) {
-            dd($e->getMessage());
+            return redirect('/home');;
         }
+        // Only allow people with @bitkub.com to login
+        if (explode("@", $user->email)[1] !== 'bitkub.com') {
+            return redirect()->to('/home');
+        }
+        // check if they're an existing user
+        $existingUser = User::where('email', $user->email)->first();
+        if ($existingUser) {
+            // log them in
+            auth()->login($existingUser, true);
+        } else {
+            // create a new user
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id' => $user->id,
+                'password' => encrypt('123456dummy')
+            ]);
+            auth()->login($newUser, true);
+        }
+        return redirect('/home');
     }
 }
